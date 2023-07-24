596f35f4d705c3b9713f8bff066e6d4ed2b0d1a0
Danish A <42688495+danisha138@users.noreply.github.com>
2023-07-24 22:41:39 +0530

    Update Exchange-Seats-of-Students.py
    added sparksql code

--- Exchange-Seats-of-Students.py
+++ Exchange-Seats-of-Students.py
@@ -19,3 +19,35 @@ exchangeDF.display()
 exchangeDF=exchangeDF.drop("original_seating").withColumnRenamed("exchanged_seating","student")
 
 exchangeDF.show()
+
+
+#################SPARK SQL##################
+df.createOrReplaceTempView("df")
+select * from df;
+
+--Exchange seat for the first 2 student----
+select id,name as original_seating,
+case when id % 2=1 THEN coalesce(lead(name) over (order by id),name)
+        when id % 2=0 THEN coalesce(lag(name) over (order by id),name)
+		else name
+end as exchanged_seating
+from df;
+
+--Exchange seat for the first 2 student----
+
+select id,name as original_seating,
+case when id=1 THEN lead(name) over (order by id)
+     when id=2 THEN lag(name) over (order by id)
+else
+name
+end as exchanged_seating
+from df;
+
+
+--Without window function--- 
+
+select s1.*,s1.id,s2.id-1 ,s3.id+1,
+s2.name as next,s3.name as prev 
+from df s1 
+left join df s2 on s1.id=s2.id-1 
+left join df s3 on s1.id=s3.id+1
